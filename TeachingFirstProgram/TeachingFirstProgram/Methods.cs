6f35b66c7cd0bc91f4549e6075798134955ae672
Parker022 <122635699+Parker022@users.noreply.github.com>
2023-01-13 22:02:24 +0000

    Learn't basics and methods
    created a calculator using different methods and basics

---
+++ TeachingFirstProgram/TeachingFirstProgram/Methods.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachingFirstProgram
+{
+    public class Methods
+    {
+        //1. must always work until stopped manually
+        //2. must add, subtract, multiply and divide
+        public void Calculator()
+        {
+            while (true)
+            {
+                Console.WriteLine("please choose what you would like to do:\n" +
+                    "a: addition \n" +
+                    "s: subtraction \n" +
+                    "d: division \n" +
+                    "m: multiplication \n" +
+                    "e: ends the program \n");
+                string input = Console.ReadLine();
+                input = input.ToLower();
+
+                if (input == "a")
+                {
+                    int x = GetNumber();
+                    int y = GetNumber();
+                    
+                    int z = Addition(x, y);
+                    Console.WriteLine("your numbers are: " + x + " and " + y);
+                    Console.WriteLine("added they are " + z);
+                }
+                else if (input == "s")
+                {
+                    int x = GetNumber();
+                    int y = GetNumber();
+
+                    int z = Subtraction(x, y);
+                    Console.WriteLine("your numbers are: " + x + " and " + y);
+                    Console.WriteLine("subtracted they are " + z);
+                }
+                else if (input == "d")
+                {
+                    int x = GetNumber();
+                    int y = GetNumber();
+
+                    float z = Division(x, y);
+                    Console.WriteLine("your numbers are: " + x + " and " + y);
+                    Console.WriteLine("divided they are " + z);
+                }
+                else if (input == "m")
+                {
+                    int x = GetNumber();
+                    int y = GetNumber();
+
+                    int z = Multiplication(x, y);
+                    Console.WriteLine("your numbers are: " + x + " and " + y);
+                    Console.WriteLine("multiplied they are " + z);
+                }
+                else if (input == "e")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("not an option");
+                    
+                }
+            }
+
+        }
+
+        public int GetNumber()
+        {
+            int result2 = 0;
+            bool n1 = true;
+            do
+            {
+                Console.WriteLine("Enter number: ");
+                string input = Console.ReadLine();
+                n1 = int.TryParse(input, out result2);
+
+                if (n1 == false)
+                {
+                    Console.WriteLine("That's not a number");
+                }
+            } while (n1 == false);
+
+            return result2;
+        }
+
+
+
+        //public void GetInput()
+        //{
+        //    while(true)
+        //    {
+        //        Console.WriteLine("hello world");
+
+        //        string string1 = Console.ReadLine();
+        //        string completeString = string1.ToLower();
+        //        completeString = completeString.Trim();
+
+        //        bool isInt = int.TryParse(string1, out int result1);
+
+        //        if (isInt == true)
+        //        {
+        //            GetInput2(result1);
+        //        }
+        //        else if (completeString == "end")
+        //        {
+        //            break;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("thats not a number");
+        //        }
+        //        int answer = Addition(1,6);
+        //        string answerString = answer.ToString();
+        //        Console.WriteLine("Answer is: " + answerString);
+        //    }
+        //}
+        //public void GetInput2(int int1)
+        //{
+        //    int int2 = int1 + 10;
+        //    Console.WriteLine(int2.ToString());
+        //}
+
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public float Division(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                return 0;
+            }
+            return a / b;
+        }
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
--- TeachingFirstProgram/TeachingFirstProgram/Program.cs
+++ TeachingFirstProgram/TeachingFirstProgram/Program.cs
@@ -1,63 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//// See https://aka.ms/new-console-template for more information
 
-//example of a generic variable
-var variable = "Hello world";
+////example of a generic variable
+//using TeachingFirstProgram;
 
-//example of variable types
-char charVar = 'c'; //Char = a single character
-string stringVar = "string"; //String = a collection of characters pieced together
-int intVar = 10; //Integer = a whole number
-float floatVar = 1.2f; //Float = floating-point number (aka a whole number that has a decimal)
-double doubleVar = 1.2; //Double = double-precision floating-point number (aka a whole number that has a decimal)
-decimal decimalVar = 1.2M; //Decimal = decimal number
-bool boolVar = false; //Bool = a boolean (true or false)
+//var variable = "Hello world";
 
-//example of writing to the console
-Console.WriteLine(variable);
+////example of variable types
+//char charVar = 'c'; //Char = a single character
+//string stringVar = "string"; //String = a collection of characters pieced together
+//int intVar = 10; //Integer = a whole number
+//float floatVar = 1.2f; //Float = floating-point number (aka a whole number that has a decimal)
+//double doubleVar = 1.2; //Double = double-precision floating-point number (aka a whole number that has a decimal)
+//decimal decimalVar = 1.2M; //Decimal = decimal number
+//bool boolVar = false; //Bool = a boolean (true or false)
 
-//example of parsing values and reading values
-string input = Console.ReadLine();
-bool isInt = int.TryParse(input, out int value);
+////example of writing to the console
+//Console.WriteLine(variable);
 
-//example of if, else if and else
-if (isInt)
-{
-    Console.WriteLine("Your input was an integer: " + value);
-}
-else if (string.IsNullOrEmpty(input))
-{
-    Console.WriteLine("Your input was empty!");
-}
-else
-{
-    Console.WriteLine("Your input was: " + input);
-}
+////example of parsing values and reading values
+//string input = Console.ReadLine();
+//bool isInt = int.TryParse(input, out int value);
 
-//example of loops
-bool whileLoopBool = true;
-while (whileLoopBool) //This will loop unless whatever the condition is = false. This means if there was an int x = 2 and the while loop condition is (x == 2) then it will loop until x is changed
-{
-    var ans = Console.ReadLine();
-    if (string.IsNullOrEmpty(ans))
-    {
-        break; //Can use break or set the value of whileLoopBool to = false
-    }
-}
+////example of if, else if and else
+//if (isInt)
+//{
+//    Console.WriteLine("Your input was an integer: " + value);
+//}
+//else if (string.IsNullOrEmpty(input))
+//{
+//    Console.WriteLine("Your input was empty!");
+//}
+//else
+//{
+//    Console.WriteLine("Your input was: " + input);
+//}
 
-int forLoopInt = 10;
-for (int i = 0; i < forLoopInt; i++) //The variable i is declared here, the loop condition is then set after and then what happens to i is set afterwards.
-{
-    Console.WriteLine(i); //As shown on the console i is incremented each time it loops through.
-}
+////example of loops
+//bool whileLoopBool = true;
+//while (whileLoopBool) //This will loop unless whatever the condition is = false. This means if there was an int x = 2 and the while loop condition is (x == 2) then it will loop until x is changed
+//{
+//    var ans = Console.ReadLine();
+//    if (string.IsNullOrEmpty(ans))
+//    {
+//        break; //Can use break or set the value of whileLoopBool to = false
+//    }
+//}
 
-bool doWhileBool = false;
-do //The functionality in this loop will run once always.
-{
-    Console.WriteLine("This loop ran at least once");
-} while (doWhileBool); //The condition of the loop will be checked only after the functionality within the loop has run once. The condition must be true for it to loop again.
+//int forLoopInt = 10;
+//for (int i = 0; i < forLoopInt; i++) //The variable i is declared here, the loop condition is then set after and then what happens to i is set afterwards.
+//{
+//    Console.WriteLine(i); //As shown on the console i is incremented each time it loops through.
+//}
+
+//bool doWhileBool = false;
+//do //The functionality in this loop will run once always.
+//{
+//    Console.WriteLine("This loop ran at least once");
+//} while (doWhileBool); //The condition of the loop will be checked only after the functionality within the loop has run once. The condition must be true for it to loop again.
 
 //Next stage is basic methods
 //Exercise is using basic methods and the new variables and functionalities to make a simple calculator
 
 //Next stage is learning Lists, Arrays, Dictionaries and ForEach loops
 //Exercise is using methods, take in 8 values into a list, order them by ascending in a method and then write them 1 by 1 to the console.
+
+List<string> strings = new List<string>() { "other", "dumb", "something" };
+
+foreach (var item in strings)
+{
+    Console.WriteLine("This item string is: " + item);
+}
+
+for (int i = 0; i < strings.Count; i++)
+{
+    Console.WriteLine("This item string is: " + strings[i]);
+}
